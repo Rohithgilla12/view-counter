da0e3d4af1f41a047c2f4cd4957a7910177f73c9
rohithgilla <gillarohith1@gmail.com>
2021-12-05 20:42:02 +0530

    Create a view counter

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { Container } from "@chakra-ui/react";
+import { Box, Center } from "@chakra-ui/react";
 
 import { DarkModeSwitch } from "./DarkModeSwitch";
 import { ReactNode } from "react";
@@ -10,15 +10,17 @@ type Props = {
 };
 
 const Layout = ({ children, title = "This is the default title" }: Props) => (
-  <Container centerContent maxH="100%" maxW="100%" p={8}>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <DarkModeSwitch />
-    {children}
-  </Container>
+  <Center>
+    <Box maxH="100%" maxW="80%" p={8}>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <DarkModeSwitch />
+      {children}
+    </Box>
+  </Center>
 );
 
 export default Layout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,93 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
-import { Auth } from "@supabase/ui";
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Heading,
+  Input,
+  SimpleGrid,
+  Text,
+} from "@chakra-ui/react";
+import { Auth, Button } from "@supabase/ui";
 import React from "react";
-import { ColumnContainer } from "../components/ColumnContainer";
 import Layout from "../components/Layout";
 import { supabase } from "../utils/supabase";
 
 const IndexPage = () => {
   const { user } = Auth.useUser();
 
-  console.log(user);
+  if (user) {
+    return (
+      <Layout title="Home | Create">
+        <Heading textAlign="center" as="h2" color="#26b37d">
+          Create View Counter
+        </Heading>
+        <SimpleGrid mt={8} columns={{ sm: 1, md: 2, lg: 2 }} spacing={8}>
+          <Box>
+            <form
+              onSubmit={async (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                const slug = (e.target as any).slug.value;
+                const url = (e.target as any).url.value;
 
+                const { data, error } = await supabase.from("asset").insert([
+                  {
+                    slug: slug,
+                    url: url,
+                    owner: user.id,
+                  },
+                ]);
+
+                if (error) {
+                  // Handle error
+                  console.log(error);
+                } else {
+                  console.log(data);
+                }
+              }}
+            >
+              <FormControl id="url">
+                <FormLabel>URL</FormLabel>
+                <Input name="url" type="url" />
+                <FormHelperText>
+                  This is optional, it is for the url which you want to track
+                  views
+                </FormHelperText>
+              </FormControl>
+              <FormControl id="slug">
+                <FormLabel>Slug</FormLabel>
+                <Input name="slug" type="text" />
+                <FormHelperText>
+                  This is optional too, if you don't provide a slug, we generate
+                  an id for you
+                </FormHelperText>
+              </FormControl>
+              <Box h={4} />
+              <Button type="primary" block htmlType="submit">
+                Create
+              </Button>
+            </form>
+          </Box>
+          <Box>
+            <Text textAlign="center" fontSize="lg">
+              Embed URL
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Layout>
+    );
+  }
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <SimpleGrid columns={2} spacing={10}>
-        <Text fontSize="xl">(xl) In love with React & Next</Text>
-        <Auth
-          supabaseClient={supabase}
-          onlyThirdPartyProviders
-          providers={["google"]}
-        />
-      </SimpleGrid>
+    <Layout title="Home | Login">
+      <Heading textAlign="center" as="h2" color="#26b37d">
+        Login
+      </Heading>
+      <Auth
+        supabaseClient={supabase}
+        onlyThirdPartyProviders
+        providers={["google"]}
+      />
     </Layout>
   );
 };
