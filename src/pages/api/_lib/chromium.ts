cc4344b74366de6955175135d932a2f7636fbe09
rohithgilla <gillarohith1@gmail.com>
2021-12-13 10:49:11 +0530

    Add option to generate as image

---
+++ src/pages/api/_lib/chromium.ts
@@ -0,0 +1,31 @@
+import core from "puppeteer-core";
+import { getOptions } from "./options";
+
+let _page: core.Page | null;
+
+export type FileType = "png" | "jpeg";
+
+async function getPage(isDev: boolean) {
+  if (_page) {
+    return _page;
+  }
+  const options = await getOptions(isDev);
+  const browser = await core.launch(options);
+  _page = await browser.newPage();
+  return _page;
+}
+
+const MAINFRAME_SELECTOR = "body > div";
+
+export async function getScreenshot(
+  html: string,
+  type: FileType,
+  isDev: boolean
+) {
+  const page = await getPage(isDev);
+  await page.setViewport({ width: 140, height: 120 });
+  await page.setContent(html);
+  const element = await page.$(MAINFRAME_SELECTOR);
+  const file = await element.screenshot({ type });
+  return file;
+}
---
+++ src/pages/api/view/image/[id].ts
@@ -0,0 +1,123 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { supabase } from "../../../../utils/supabase";
+import { getScreenshot } from "../../_lib/chromium";
+
+const isDev = !process.env.AWS_REGION;
+
+const getCommonCSS = () => {
+  return `
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+
+    html{
+      width: 120px;
+      height:120px;
+    }
+
+    body {
+        color: white;
+        // background-size: 100px 100px;
+        display: flex;
+        text-align: center;
+        align-items: center;
+        justify-content: center;
+        font-family: 'Inter', sans-serif;
+        font-weight: 400;
+        font-size: 24px;
+        margin: 0;
+        padding: 0;
+    }
+    * {
+        box-sizing: border-box;
+    }
+    h1, h2, h3, h4, h5, p {
+      margin: 0;
+    }
+    code {
+        color: #D400FF;
+        font-family: 'Vera';
+        white-space: pre-wrap;
+        letter-spacing: -5px;
+    }
+    code:before, code:after {
+        content: '\`';
+    }
+
+    .mainFrame{
+      background : #093145;
+      padding: 8px;
+    }
+`;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+
+  let html = "";
+
+  const { error } = await supabase.from("analytics").insert([
+    {
+      asset: id,
+    },
+  ]);
+
+  if (error) {
+    console.log(error);
+    let errorMessage = error.message;
+    if (error.code === "23503") {
+      errorMessage =
+        "Can you please check the URL?, it looks like you are trying to access the asset which is not available.\nIf the error still persists contact the Dev";
+    }
+    html = `<!DOCTYPE html>
+<html>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width">
+    <style>
+        ${getCommonCSS()}
+    </style>
+    <body>
+      <code>${errorMessage}</code>      
+    </body>
+</html>`;
+  } else {
+    const { count } = await supabase
+      .from("analytics")
+      .select("asset", { count: "exact" })
+      .eq("asset", id);
+
+    const viewString = count === 1 ? "View" : "Views";
+
+    html = `<!DOCTYPE html>
+  <html>
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width">
+     <style>
+         ${getCommonCSS()}
+     </style>
+     <body>
+      <div class="mainFrame">
+       <p>${viewString} ${count}</p>
+      </div>
+     </body>
+  </html>`;
+  }
+
+  const fileType = "png";
+  try {
+    const file = await getScreenshot(html, fileType, isDev);
+    res.statusCode = 200;
+    res.setHeader("Content-Type", `image/${fileType}`);
+    res.setHeader(
+      "Cache-Control",
+      `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`
+    );
+    res.end(file);
+  } catch (e) {
+    res.statusCode = 500;
+    res.setHeader("Content-Type", "text/html");
+    res.end("<h1>Internal Error</h1><p>Sorry, there was a problem</p>");
+    console.error(e);
+  }
+}
